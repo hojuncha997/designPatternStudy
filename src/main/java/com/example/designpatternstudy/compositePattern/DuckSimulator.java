92cfd3160b6b124f0d54782b5291cc9ad81e3084
Hojun Cha <hojun.cha997@gmail.com>
2022-10-14 22:34:19 +0900

    Composite Pattern: 2. Decorator 패턴 추가

--- src/main/java/com/example/designpatternstudy/compositePattern/DuckSimulator.java
+++ src/main/java/com/example/designpatternstudy/compositePattern/DuckSimulator.java
@@ -1,3 +1,5 @@
+//DuckSimulator.java
+
 package com.example.designpatternstudy.compositePattern;
 
 public class DuckSimulator {
@@ -8,24 +10,42 @@ public class DuckSimulator {
     }
 
     void simulate(){
-        QuackableInterfaceForCompositePattern mallardDuck = new MallardDuck();
-        QuackableInterfaceForCompositePattern redheadDuck = new RedheadDuck();
-        QuackableInterfaceForCompositePattern duckCall = new DuckCall();
-        QuackableInterfaceForCompositePattern rubberDuck = new RubberDuck();
+        //인터페이스를 새로 생성할 때마다 새로운 데코레이터로 감싼다.
+        QuackableInterfaceForCompositePattern mallardDuck
+                = new QuackCountingDecorator(new MallardDuck());
+
+        QuackableInterfaceForCompositePattern redheadDuck
+                = new QuackCountingDecorator(new RedheadDuck());
+
+        QuackableInterfaceForCompositePattern duckCall
+                = new QuackCountingDecorator(new DuckCall());
+
+        QuackableInterfaceForCompositePattern rubberDuck
+                = new QuackCountingDecorator(new RubberDuck());
         //GooseAdapter를 사용해서 오리가 됨
         QuackableInterfaceForCompositePattern gooseDuck = new GooseAdapter(new Goose());
 
-        System.out.println("\n오리 시뮬레이션 게임 (+ 거위 어댑터)");
+        //GooseAdapter오리도 데코레이터 사용
+        QuackableInterfaceForCompositePattern gooseDuckDeco
+                = new QuackCountingDecorator(new GooseAdapter(new Goose()));
 
-        simulate(mallardDuck);
-        simulate(redheadDuck);
-        simulate(duckCall);
-        simulate(rubberDuck);
+
+
+        System.out.println("\n오리 시뮬레이션 게임 (+ 데코레이터)");
+
+        simulate(mallardDuck); //1
+        simulate(redheadDuck); //2
+        simulate(duckCall); //3
+        simulate(rubberDuck); //4
         //거위 시뮬레이트트
        simulate(gooseDuck);
+       simulate(gooseDuckDeco);//5
+
+        System.out.println("오리가 소리 낸 횟수:" +
+                QuackCountingDecorator.getQuacks() + " 번");
 
     }
-
+    //데코레이터도 QuackableInterfaceForCompositePatter 인터페이스이다.
     void simulate(QuackableInterfaceForCompositePattern duck){
         duck.quack();
     }
---
+++ src/main/java/com/example/designpatternstudy/compositePattern/QuackCountingDecorator.java
@@ -0,0 +1,24 @@
+//QuackCountingDecorator.java
+package com.example.designpatternstudy.compositePattern;
+//데코레이터. 어댑터와 마찬가지로 타겟 인터페이스 구현 필요
+public class QuackCountingDecorator implements QuackableInterfaceForCompositePattern {
+    //감싸고자 하는 객체의 레퍼런스를 저장하는 인스턴스 변수 필요
+    QuackableInterfaceForCompositePattern duck;
+    static int numberOfQuacks;
+
+    //생성자: 생성자에서 감싸고 있는 타겟 인터페이스의 레퍼런스를 가져온다.
+    public QuackCountingDecorator(QuackableInterfaceForCompositePattern duck){
+        this.duck = duck;
+    }
+
+    @Override
+    public void quack() {//호출 시 내부의 객체에게 동작을 위임.
+        duck.quack();
+        numberOfQuacks++;
+    }
+
+    public static int getQuacks(){
+        return numberOfQuacks;
+    }
+
+}
